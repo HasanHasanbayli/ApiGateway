8e364ef66b1a1896a3909373b1e9d8e08950d687
hasanhasanbayli <hasanhasanbeyli@gmail.com>
2023-03-28 23:03:55 +0400

    Initial

---
+++ ApiGateway/HttpService.cs
@@ -0,0 +1,94 @@
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+namespace ApiGateway;
+
+public class HttpService
+{
+    private readonly HttpClient _httpClient;
+
+    public HttpService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<TResult> Post<T, TResult>(T arg, string uri, string? requestUri = null,
+        Dictionary<string, string>? headers = null, CancellationToken cancellationToken = default)
+    {
+        HttpRequestMessage request = CreateHttpRequest(HttpMethod.Post, uri, requestUri, arg, headers);
+        HttpResponseMessage response = await _httpClient.SendAsync(request, cancellationToken);
+
+        return await HandleResponse<TResult>(response);
+    }
+
+    public async Task<T> Get<T>(string uri, string? requestUri = null, Dictionary<string, string>? headers = null,
+        CancellationToken cancellationToken = default)
+    {
+        HttpRequestMessage request = CreateHttpRequest(HttpMethod.Get, uri, requestUri, headers: headers);
+        HttpResponseMessage response = await _httpClient.SendAsync(request, cancellationToken);
+
+        return await HandleResponse<T>(response);
+    }
+
+
+    public async Task<TResult> Put<T, TResult>(T arg, string uri, string? requestUri = null,
+        Dictionary<string, string>? headers = null, CancellationToken cancellationToken = default)
+    {
+        HttpRequestMessage request = CreateHttpRequest(HttpMethod.Put, uri, requestUri, arg, headers);
+        HttpResponseMessage response = await _httpClient.SendAsync(request, cancellationToken);
+
+        return await HandleResponse<TResult>(response);
+    }
+
+    public async Task Delete(string uri, string? requestUri = null, Dictionary<string, string>? headers = null,
+        CancellationToken cancellationToken = default)
+    {
+        HttpRequestMessage request = CreateHttpRequest(HttpMethod.Delete, uri, requestUri, headers: headers);
+        HttpResponseMessage response = await _httpClient.SendAsync(request, cancellationToken);
+
+        await HandleResponse(response);
+    }
+
+    private HttpRequestMessage CreateHttpRequest(HttpMethod method, string uri, string? requestUri = null,
+        object? requestBody = null, Dictionary<string, string>? headers = null)
+    {
+        HttpRequestMessage request = new(method, !string.IsNullOrEmpty(requestUri) ? requestUri : uri);
+
+        if (headers != null)
+            foreach (var header in headers)
+                request.Headers.Add(header.Key, header.Value);
+
+        if (requestBody != null)
+        {
+            string jsonBody = JsonSerializer.Serialize(requestBody);
+            request.Content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+        }
+
+        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        return request;
+    }
+
+    private async Task HandleResponse(HttpResponseMessage response)
+    {
+        if (!response.IsSuccessStatusCode)
+        {
+            string errorBody = await response.Content.ReadAsStringAsync();
+            throw new Exception(errorBody);
+        }
+    }
+
+    private async Task<T> HandleResponse<T>(HttpResponseMessage response)
+    {
+        if (!response.IsSuccessStatusCode)
+        {
+            string errorBody = await response.Content.ReadAsStringAsync();
+            throw new Exception(errorBody);
+        }
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        return JsonSerializer.Deserialize<T>(responseBody)!;
+    }
+}
---
+++ ApiGateway/Program.cs
@@ -0,0 +1,27 @@
+﻿using ApiGateway;
+
+const string uri = "https://jsonplaceholder.typicode.com";
+
+// Creating an instance of HttpService with HttpClient
+using HttpClient client = new();
+HttpService httpService = new(client);
+
+// Posting HTTP requests using HttpService
+var post = new { title = "foo", body = "bar", userId = 1 };
+object createdPost = await httpService.Post<object, object>(post, uri + "/posts");
+Console.WriteLine($"Created post: {createdPost}");
+
+// Getting HTTP requests using HttpService
+var getPosts = await httpService.Get<IEnumerable<object>>(uri + "/posts");
+Console.WriteLine("All posts:");
+foreach (object item in getPosts) Console.WriteLine(item);
+
+// Putting HTTP requests using HttpService
+var put = new { id = 1, title = "foo", body = "bar", userId = 1 };
+object updatedPost = await httpService.Put<object, object>(put, uri + "/posts/1");
+Console.WriteLine($"Updated post: {updatedPost}");
+
+// Deleting HTTP requests using HttpService
+await httpService.Delete(uri + "/posts/1");
+
+Console.ReadLine();
